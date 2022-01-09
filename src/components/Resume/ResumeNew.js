0c4e973d7754f39d10f6babfa18e49c88f442e7e
SujalShah3234 <shahc9437@gmail.com>
2022-01-09 21:55:38 +0530

    Fix: Button

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -22,25 +22,18 @@ function ResumeNew() {
     <div>
       <Container fluid className="resume-section">
         <Particle />
-        <Row style={{ justifyContent: 'center', position: 'relative' }}>
-          <Button variant="primary" href={pdf} target="_blank">
-            <AiOutlineDownload />
-            &nbsp;Download CV
-          </Button>
-        </Row>
-
         <Row className="resume">
           <Document file={resumeLink}>
             <Page pageNumber={1} scale={width > 786 ? 1.7 : 0.6} />
           </Document>
         </Row>
 
-        <Row style={{ justifyContent: 'center', position: 'relative' }}>
+        <div className="d-flex justify-content-center">
           <Button variant="primary" href={pdf} target="_blank">
             <AiOutlineDownload />
             &nbsp;Download CV
           </Button>
-        </Row>
+        </div>
       </Container>
     </div>
   );
